98de2574a687f0d95ae03212b8ba1ea6cd4ff5e1
Patricio Borghi <160797588+PatotQ@users.noreply.github.com>
2024-12-24 09:25:00 -0300

    hangman.rb

--- hangman.rb
+++ hangman.rb
@@ -1,12 +1,9 @@
-# hangman.rb
 
 # Word list
 words = ["ruby", "developer", "programming", "hangman", "terminal"]
 
-# Choose a random word
 secret_word = words.sample
 
-# Initialize game state
 hidden_word = "_" * secret_word.length
 attempts_left = 6
 guessed_letters = []
@@ -26,7 +23,6 @@ while attempts_left > 0 && hidden_word.include?("_")
     puts "Good job! '#{guess}' is in the word."
     guessed_letters << guess
 
-    # Replace underscores with the guessed letter
     secret_word.chars.each_with_index do |char, index|
       hidden_word[index] = char if char == guess
     end
